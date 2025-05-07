a7f17196ec701b8cbfd136fa595dd6c866fd055c
Phillip Green <phillip.g.green@outlook.com>
2025-05-07 14:31:03 -0400

    Refactor color scheme: replaced white with cream, cream with sand
    Replaced all instances of white with the new cream color (#f3efe0) for a softer contrast. Updated previous cream usage to use the new sand tone (#ddd4c2) for improved visual hierarchy and readability. Also updated Tailwind config and all relevant components and pages to reflect the changes.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
 		extend: {
 			colors: {
-				primary: '#22a39f',
-				dark: '#222222',
-				cream: '#f3efe0',
+				primary: "#22a39f",
+				dark: "#222222",
+				cream: "#f3efe0",
+				sand: "#ddd4c2",
 			},
 			fontFamily: {
-				sans: ['"Inter"', 'sans-serif'],
+				sans: ['"Inter"', "sans-serif"],
 			},
 		},
 	},
